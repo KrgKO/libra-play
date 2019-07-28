1e87d8dfe6569b6d316e089e5500a100244be486
KrgKo <kowpun123@gmail.com>
2019-07-29 00:11:26 +0700

    modify grpc decode

--- libra-grpc/app.js
+++ libra-grpc/app.js
@@ -35,40 +35,40 @@ function main(cmd, params) {
         return;
       }
 
-      const response = r.response_items[0][`${cmd}_response`];
-      if (response.signed_transaction_with_proof) {
-        console.log("===> sender");
-        protobuf.load(
-          path.join(__dirname, "./proto/transaction.proto"),
-          (err, root) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
+      // Need difference protobuf to decode
+      console.log(JSON.stringify(r.response_items));
+      
+      // Support only get_account_transaction_by_sequence_number_request
+      //
+      // const response = r.response_items[0][`${cmd}_response`];
+      // if (response.signed_transaction_with_proof) {
+      //   console.log("===> sender");
+      //   protobuf.load(
+      //     path.join(__dirname, "./proto/transaction.proto"),
+      //     (err, root) => {
+      //       if (err) {
+      //         console.error(err);
+      //         return;
+      //       }
 
-            const extractor = root.lookupType("types.RawTransaction");
-            const raw_msg = extractor.decode(
-              r.response_items[0][`${cmd}_response`]
-                .signed_transaction_with_proof.signed_transaction.raw_txn_bytes
-            );
-            console.log(raw_msg);
-          }
-        );
-      } else if (response.proof_of_current_sequence_number) {
-        console.log("===> receiver");
-        console.log(response.proof_of_current_sequence_number);
-      }
+      //       const extractor = root.lookupType("types.RawTransaction");
+      //       const raw_msg = extractor.decode(
+      //         r.response_items[0][`${cmd}_response`]
+      //           .signed_transaction_with_proof.signed_transaction.raw_txn_bytes
+      //       );
+      //       console.log(raw_msg);
+      //     }
+      //   );
+      // } else if (response.proof_of_current_sequence_number) {
+      //   console.log("===> receiver");
+      //   console.log(response.proof_of_current_sequence_number);
+      // }
     }
   );
 }
 
-// 56cfd6d18e6b01713e01508d2da07184939ca8dccf52f6855066eb5ab5292172 - sender
-// 935686ca53960b4a4b1b2d0b6da3488862e36d2cfab34c0a705811d5073ca403 - receiver
-main("get_account_transaction_by_sequence_number", {
-  account: Buffer.from(
-    "935686ca53960b4a4b1b2d0b6da3488862e36d2cfab34c0a705811d5073ca403",
-    "hex"
-  ),
-  sequence_number: 0,
+main("get_transactions", {
+  start_version: 1,
+  limit: 10,
   fetch_events: true
 });
